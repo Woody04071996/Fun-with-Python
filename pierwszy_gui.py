2dbfe96a3e3e023592354bc0f18996d5d11314f9
Woody04071996 <95613654+Woody04071996@users.noreply.github.com>
2023-09-06 02:16:30 +0200

    Add files via upload

---
+++ pierwszy_gui.py
@@ -0,0 +1,14 @@
+#interfejs gui
+#tworznie okien
+
+from tkinter import *
+
+#tworzenie okna glownego
+root = Tk()
+
+#modyfikacja okna
+root.title("Zwykly interfejs GUI")
+root.geometry("350x150")
+
+#uruchom petle zdarzen
+root.mainloop()
